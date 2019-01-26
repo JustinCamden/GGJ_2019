02c4e425300d7c1ea0a6ba53b0ed1121f716b470
Sam Celeste <scceleste@gmail.com>
2019-01-26 01:29:05 -0800

    ADDED DIALOGUE SYSTEEEEEEEM
    Interface with it by filling the "conversation windows" list and then doing the stuff I will show people how iof tbhey want to write dialogue

--- Assets/Scripts/BaseInteraction.cs
+++ Assets/Scripts/BaseInteraction.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using InControl;
 
 public class BaseInteraction : MonoBehaviour {
 
 	public GameObject interactSprite;
+	[Tooltip("Whether or not this interaction starts a minigame")]
 	public bool shouldStartMinigame;
+	[Tooltip("Whether or not this interaction has associated dialogue")]
+	public bool shouldRunDialogue;
+	[Tooltip("used for seeing if the player should be able to hit space and interact")]
+	public bool interactEnabled = true;
 
+	public List<DialogueRunner.ConversationWindow> conversationWindows;
+	
+	
+	private InputDevice controller;
+	
 	public virtual void Interact () {
 		// do stuff here!
 		// turn lights on/off, play sound, trigger animations
@@ -17,6 +28,12 @@ public class BaseInteraction : MonoBehaviour {
 		// start a minigame here 
 	}
 
+	void Update()
+	{
+		//grab controller
+		controller = InputManager.ActiveDevice;
+	}
+	
 	private void OnTriggerEnter(Collider other)
     {
 		// once player enters, set sprite on 
@@ -25,15 +42,41 @@ public class BaseInteraction : MonoBehaviour {
 
 	private void OnTriggerStay (Collider other)
 	{
-		if (Input.GetKeyDown(KeyCode.Space))
+		// waits for user input 
+		if (interactEnabled)
 		{
-			Interact();
-
-			if (shouldStartMinigame)
+			if (!interactSprite.activeInHierarchy)
 			{
-				StartMinigame();
+				interactSprite.SetActive(true);
+			}
+
+			if (Input.GetKeyDown(KeyCode.Space) || controller.Action1)
+			{
+				// checks to see if it should Start a Minigame, Start a Dialogue, or just Interact. 
+				if (shouldStartMinigame)
+				{
+					StartMinigame();
+				}
+				else if (shouldRunDialogue)
+				{
+					DialogueStart();
+					interactEnabled = false;
+				}
+				else
+				{
+					Interact();
+				}
 			}
 		}
+		else
+		{
+			if (interactSprite.activeInHierarchy)
+			{
+				interactSprite.SetActive(false);
+			}
+		}
+		
+
 	}
 
     private void OnTriggerExit(Collider other)
@@ -41,4 +84,25 @@ public class BaseInteraction : MonoBehaviour {
         // once exits sets arrow to false 
         interactSprite.SetActive(false);
     }
+
+	public void DialogueStart()
+	{
+		DialogueRunner.Instance.UIWindow.SetActive(true);
+		DialogueRunner.Instance.currentWindows = conversationWindows;
+		DialogueRunner.Instance.RunConversation();
+		DialogueRunner.Instance.currentWindow = 0;
+
+	}
+
+	public void DialogueInteract()
+	{
+		if (shouldStartMinigame)
+		{
+			StartMinigame();
+		}
+		else
+		{
+			Interact();
+		}
+	}
 }
