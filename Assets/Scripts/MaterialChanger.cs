044c46eb961b0af3a54ce19a018a52e5d1dae6d5
Sam Celeste <scceleste@gmail.com>
2019-01-26 13:55:47 -0800

    Stub of script for material changing

---
+++ Assets/Scripts/MaterialChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : BaseInteraction {
+
+	public List<Material> paints;
+	public List<GameObject> objectsToPaint;
+	private int currentMat = 0;
+
+	public override void Interact()
+	{
+//		if (currentMat  paints.)
+	}
+
+	public IEnumerator ChangeMat(Material mat)
+	{
+		yield return null;
+	}
+}
