a79a4ae44c1b888637a0791069dc40c157209180
Sam Celeste <scceleste@gmail.com>
2019-01-26 19:13:35 -0800

    push 4 life

---
+++ Assets/Scripts/PosterGameInteraction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class PosterGameInteraction : BaseInteraction
+{
+    public PosterGame pG;
+    
+    public override void Interact()
+    {
+        pG.PosterMinigame();
+    }
+}
