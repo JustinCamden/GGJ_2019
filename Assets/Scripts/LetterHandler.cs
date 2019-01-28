a73f25cb14dab738d337c94e40efa145c44f34b9
Sky <sky@skycopeland.com>
2019-01-27 16:27:02 -0800

    Letter Handler v2

--- Assets/Scripts/LetterHandler.cs
+++ Assets/Scripts/LetterHandler.cs
@@ -23,19 +23,10 @@ public class LetterHandler : MonoBehaviour {
     {
         yield return new WaitForSeconds(1f);
         letter.DOMoveY(550, 3f, true);
-        yield return new WaitForSeconds(1f);
         a.Play();
-        yield return new WaitForSeconds(5f);
-        //yield return StartCoroutine(WaitForKeyDown(KeyCode.Space));
+        yield return new WaitUntil(() => (InputManager.ActiveDevice.Action1.WasPressed == true) || Input.GetKeyDown(KeyCode.Space));
         letter.DOMoveY(-2000, 2f, true);
         yield return new WaitForSeconds(1f);
         i.DOFade(0, 3);
-    }
-
-    IEnumerator WaitForKeyDown(KeyCode keyCode)
-    {
-        while (!Input.GetKeyDown(keyCode) || !InputManager.ActiveDevice.Action1)
-            yield return null;
-        yield return new WaitForFixedUpdate();
     }
 }
